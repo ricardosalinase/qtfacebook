1a2a0e7445c8dc153d0abca66a0641a3f10e0e29
roach <roach@mostlyharmless.net>
2010-02-02 14:43:45 +0000

    Mucking with data structures some more ... still haven't got the design I want

---
+++ gui/notificationlabel.h
@@ -0,0 +1,24 @@
+#ifndef NOTIFICATIONLABEL_H
+#define NOTIFICATIONLABEL_H
+
+#include <QLabel>
+
+#include "data/notification.h"
+
+namespace GUI {
+
+class NotificationLabel : public QLabel
+{
+Q_OBJECT
+public:
+    NotificationLabel(DATA::Notification *n, QWidget *parent = 0);
+    //DATA::Notification * getNotification();
+    //void setNotification(DATA::Notification *n);
+private:
+    //DATA::Notification *m_notification;
+
+};
+
+} // namespace GUI
+
+#endif // NOTIFICATIONLABEL_H
